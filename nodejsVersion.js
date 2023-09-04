47f450261ca6452b0e3d2902694f3cbb66c38b19
MathengeNewton <rexynewton79@gmail.com>
2023-09-04 18:03:25 +0300

    Node version of code challenge

---
+++ nodejsVersion.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function fibonacciSequence(count) {
+  const output = [];
+  if (count == 1) {
+    output.push(0);
+    console.log(output);
+  } else if (count == 2) {
+    output.push(0, 1);
+    console.log(output);
+  } else if (count >= 3) {
+    output.push(0, 1);
+    for (let arrayElements = 2; arrayElements < count; arrayElements++) {
+      const nthElement = output[output.length - 1] + output[output.length - 2];
+      output.push(nthElement);
+    }
+    console.log(output);
+  } else {
+    console.log("Invalid input. Please enter a positive integer.");
+  }
+
+  rl.close(); // Close the readline interface
+}
+
+rl.question('Enter a number: ', (input) => {
+  const count = parseInt(input);
+
+  if (!isNaN(count) && count > 0) {
+    fibonacciSequence(count);
+  } else {
+    console.log("Invalid input. Please enter a positive integer.");
+    rl.close();
+  }
+});
