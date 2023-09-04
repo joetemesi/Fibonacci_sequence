fb931f4a25bd980bbd1af296cc60d2825f3dd828
Joseph M. Temesi <joemunyendo@gmail.com>
2023-09-04 14:56:52 +0300

    Created fibonacci sequence function

---
+++ index.js
@@ -0,0 +1,23 @@
+var output = [];
+
+function fibonacciSequence(count) {
+    var count;
+    if (count == 1) {
+        output.push(0);
+        console.log(output);
+    } else if (count == 2) {
+        output.push (0,1);
+    } else if (count == output.length) {
+        console.log(output);
+    } else if (count >= 3) {
+        output.push (0,1);
+        for (var arrayElements = 2; arrayElements < count; arrayElements++) {
+            var nthElement = output[(output.length - 1)] + output[(output.length - 2)];
+            output.push(nthElement);
+        }
+        console.log(output);
+    }
+}
+
+var count = parseInt(prompt("Enter a number: "));
+fibonacciSequence(count);
